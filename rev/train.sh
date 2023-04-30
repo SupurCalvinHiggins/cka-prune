e1ad4e7e71b7fb0fda6cc395ecc86ab4f7b9ba6e
Calvin Higgins <calvin_higgins2@seawulf.uri.edu>
2023-04-30 14:04:35 -0400

    Added results.

---
+++ rev/train.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -t 10:00:00
+#SBATCH --nodes=1 --ntasks-per-node=4
+#SBATCH --export=NONE
+
+module load Python/3.9.6-GCCcore-11.2.0
+pip install --user torch
+pip install --user numpy
+pip install --user seaborn
+pip install --user torchvision
+python -u main_train.py --model_count 30 --dropout_rate $1
